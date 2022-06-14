6da8bf32223f72ea75c8eec6d1fffad5c39e638d
Kevin McPeak <kevmc46@gmail.com>
2022-06-13 20:28:38 -0400

    added new photo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { db } from "./firebase-config";
 import React from "react";
 import { collection, getDocs } from "firebase/firestore";
-import photo from "./assets/Wheres-Waldo-Game.jpg";
+import photo from "./assets/game_basic.png";
 import Header from "./components/Header";
 import TargetingBox from "./components/TargetingBox";
 import MainPhoto from "./components/MainPhoto";
 
 function App() {
 	let [users, setUsers] = React.useState([]);
-	let [showBox, setShowBox] = React.useState(false);
+	let [showBox, setShowBox] = React.useState(true);
+	let [characters, setCharacters] = React.useState([
+		{ name: "waldo", found: false },
+		{ name: "wizard", found: false },
+		{ name: "something", found: false },
+	]);
 	let [top, setTop] = React.useState(0);
 	let [left, setLeft] = React.useState(0);
 	let usersCollectionRef = collection(db, "users");
@@ -30,14 +35,35 @@ function App() {
 		setShowBox(true);
 	};
 
-	console.log(top, left);
+	let handleTargetBoxClick = (event) => {
+		let id = event.target.id;
+
+		setCharacters((previousCharacters) =>
+			previousCharacters.map((character) => {
+				return id === character.name
+					? { ...character, found: true }
+					: character;
+			})
+		);
+	};
+
+	console.log(characters);
 
 	return (
 		<div>
 			<Header />
 			<div className="photo-container" onClick={handlePhotoClick}>
 				<div className="waldo"></div>
-				{showBox ? <TargetingBox top={top} left={left} /> : ""}
+				{showBox ? (
+					<TargetingBox
+						top={top}
+						left={left}
+						handleTargetBoxClick={handleTargetBoxClick}
+						showBox={showBox}
+					/>
+				) : (
+					""
+				)}
 				<MainPhoto image={photo} top={top} left={left} />
 			</div>
 		</div>
--- src/components/TargetingBox.js
+++ src/components/TargetingBox.js
@@ -3,12 +3,22 @@ import React from "react";
 let TargetingBox = (props) => {
 	return (
 		<div
-			className="targeting-box"
+			className={props.showBox ? "targeting-box" : "no-targeting-box"}
 			style={{ top: props.top, left: props.left }}
 		>
-			<div className="target-option">Waldo</div>
-			<div className="target-option">Wizard</div>
-			<div className="target-option">Something</div>
+			<div
+				onClick={props.handleTargetBoxClick}
+				id="waldo"
+				className="target-option"
+			>
+				Waldo
+			</div>
+			<div id="wizard" className="target-option">
+				Wizard
+			</div>
+			<div id="something" className="target-option">
+				Something
+			</div>
 		</div>
 	);
 };
